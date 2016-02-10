6f575589cbb2bf5e508d0b8179ecaa0302dd8bdd
imad <imadsmix@gmail.com>
2016-02-10 11:40:36 +0100

    two

---
+++ serial1.5/serial1.5.ino
@@ -0,0 +1,39 @@
+/***********************************************************
+**       programa que calcula la hipotenusa               **                                               
+**       apartir de dos catets                            **
+**                                                        **
+**                                                        **
+***********************************************************/
+
+
+//*****  Includes   ****************************************
+
+
+
+//*****  Variables  ****************************************
+
+int a = 3;
+int b = 4;
+int h;
+
+//*****  Setup  ********************************************
+
+void setup() {
+serial.print("calcular la hipotenusa");
+seral.print( "catet a=");
+serial.println(a);
+seral.print( "catet b=");
+serial.println(b);
+seral.print( "catet h=");
+serial.println(a);
+
+sqrt(sqrt(pow (3,2)+pow(4,2))
+}
+
+//*****  Loop     *******************************************
+void loop() {
+
+
+
+
+}
