c31d5a8f10aac224a589051f57f7419236cacd2f
imad <imadsmix@gmail.com>
2016-02-10 10:33:29 +0100

    serial1.1

---
+++ serial1.1/serial1.1.ino
@@ -0,0 +1,29 @@
+/***********************************************************
+**                                                        **
+**                Descripcio programa                     **       
+**                                                        **
+***********************************************************/
+
+
+//*****  Includes   ****************************************
+
+
+
+//*****  Variables  ****************************************
+
+//*****  Setup  ******************************************** 
+
+void setup() {
+
+serial.begin(9600);//Es la velocitat de conexio entre arduino i el ordinador
+serial.println("hello imad");//et mostrara un mesatja de text amb un salt  de linea ,nomes s'ha executara una vegada
+
+ 
+}
+
+//*****  Loop     ******************************************* 
+void loop() {
+  
+ 
+
+}
