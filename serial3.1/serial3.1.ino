29fa9add5a7b04bbb1467bc617b40bb33acee8a0
imad <imadsmix@gmail.com>
2016-03-11 10:20:11 +0100

    2016

--- serial3.1/serial3.1.ino
+++ serial3.1/serial3.1.ino
@@ -11,35 +11,34 @@
 
 //*****  Variables  ****************************************
 
-int number= 24;
+int number = 24;
 
 //*****  Setup  ********************************************
 
 void setup() {
 
-Serial.begin (9600);
+  Serial.begin (9600);
 }
-
+// Taula ascii
 //*****  Loop     *******************************************
 void loop() {
 
-Serial.print("DEC:");
-Serial.print (number,DEC);
-Serial.print(",Bin:");
-Serial.print (number,BIN);
-Serial.print(",OCT:");
-Serial.print (number,OCT);
-Serial.print(",HEX:");
-Serial.println  (number,HEX);
-
-if (number == 100) // si numero es igual a 100 es para 
-{
-  while(true)
+  Serial.print("DEC:");
+  Serial.print (number, DEC);
+  Serial.print(",Bin:");
+  Serial.print (number, BIN);
+  Serial.print(",OCT:");
+  Serial.print (number, OCT);
+  Serial.print(",HEX:");
+  Serial.println  (number, HEX);
+  if (number == 100) // si numero es igual a 100 es para
   {
-}
+    while (true)
+    {
+    }
 
-}
- number++;  // si no es igual a 100 anira suman fins arriba 100
+  }
+  number++;  // si no es igual a 100 anira suman fins arriba 100
 }
 
 
--- serial4.1/serial4.1.ino
+++ serial4.1/serial4.1.ino
@@ -1,6 +1,6 @@
 /***********************************************************
 **                                                        **
-**                                                        **
+**            Control de temperatura                      **
 **                                                        **
 ***********************************************************/
 
@@ -17,28 +17,22 @@ void setup() {
   Serial.begin(9600);
 
   if (temp < 90) {
-    Serial.print("Aigua encara no bullint"); 
+    Serial.print("Aigua encara no bullint");
   }
-  else if ( (temp >== 90)&&(temp < 100))
+  else if ( (temp >=  90) && (temp < 100))
   {
     Serial.print("apunt de bullir");
 
   }
 
-  else if  ( temp ==100)
+ else if  ( temp == 100)
   {
     Serial.print("aigua 100");
 
   }
-
-
   else if ( temp > 100)
   {
-    Serial.print("aigua bullint ");
-
-  }
-
-
+    Serial.print("aigua bullint ");n}
 }
 
 
